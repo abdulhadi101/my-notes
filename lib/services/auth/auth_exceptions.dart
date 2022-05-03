483987399a01c9b978e47a271274c33dc5074cdc
Abdulhadi Ahmad <get2abdul101@yahoo.com>
2022-05-03 06:59:00 +0100

    auth services

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,30 @@
+// Login exceptions
+class UserNotFoundAuthException implements Exception{
+
+}
+
+class WrongPasswordAuthException implements Exception{
+  
+}
+// Register Exception
+class WeakPasswordAuthException implements Exception{
+  
+}
+
+class EmailInUseAuthException implements Exception{
+  
+}
+
+class InvalidEmailAuthException implements Exception{
+  
+}
+
+// Generic Exceptions
+
+class GenericAuthException implements Exception{
+  
+}
+
+class UserNotLoggedInAuthException implements Exception{
+  
+}
